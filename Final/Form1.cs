8729f8c13be61b70ddd4e01506dc707408395c84
2013.103281 <2013.103281@PCI-LAB-56-0DJY>
2024-01-30 11:54:02 -0600

    Reduced code

--- Final/Form1.cs
+++ Final/Form1.cs
@@ -20,39 +20,30 @@ namespace Final
         {
             OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + (Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\final.accdb")); //Checks for final.accdb on every system.
             conn.Open(); // opens connection to the database.
-            if (nameCBox.Text == "Austin Morrison-Prieur") {
-                OleDbCommand command = new OleDbCommand("SELECT * FROM users where Username='" + nameCBox.Text + "' and Password='" + passBox.Text + "'", conn); // creates the command to check the database for the password.
-                command.ExecuteNonQuery(); // executes a non-query command, or one that doesn't return a set of rows.
+ 
+            OleDbCommand command = new OleDbCommand("SELECT * FROM users where Username='" + nameCBox.Text + "' and Password='" + passBox.Text + "'", conn); // creates the command to check the database for the password.
+            command.ExecuteNonQuery(); // executes a non-query command, or one that doesn't return a set of rows.
 
-                OleDbDataReader reader = command.ExecuteReader(); // creates an oledb reader to read the database.
-                if (reader.Read() == true) // if it can see something that matches then...
-                {
-                    string user = nameCBox.Text; // Creates a variable to be used to display username on the second form.
-                    Form3 Loggus = new Form3(); // Passes said variable.
-                    this.Hide(); // Used to hide the first form.
-                    Loggus.ShowDialog(); // Used to display the second form.
-                }
-                else { MessageBox.Show("Wrong password, try again.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning); } // if not it must be the wrong password.
-
-                conn.Close();
-            }
-            else {
-                OleDbCommand command = new OleDbCommand("SELECT * FROM users where Username='" + nameCBox.Text + "' and Password='" + passBox.Text + "'", conn); // creates the command to check the database for the password.
-                command.ExecuteNonQuery(); // executes a non-query command, or one that doesn't return a set of rows.
-
-                OleDbDataReader reader = command.ExecuteReader(); // creates an oledb reader to read the database.
-                if (reader.Read() == true) // if it can see something that matches then...
+            OleDbDataReader reader = command.ExecuteReader(); // creates an oledb reader to read the database.
+            if (reader.Read() == true) // if it can see something that matches then...
+            {
+                if (nameCBox.Text != "Austin Morrison-Prieur")
                 {
                     string user = nameCBox.Text; // Creates a variable to be used to display username on the second form.
                     Form2 TBD = new Form2(user); // Passes said variable.
                     this.Hide(); // Used to hide the first form.
                     TBD.ShowDialog(); // Used to display the second form.
                 }
-                else { MessageBox.Show("Wrong password, try again.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning); } // if not it must be the wrong password.
-
-                conn.Close();
+                else 
+                {
+                    Form3 Loggus = new Form3(); // Passes said variable.
+                    this.Hide(); // Used to hide the first form.
+                    Loggus.ShowDialog(); // Used to display the second form.
+                }
             }
-            
+            else { MessageBox.Show("Wrong password, try again.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning); } // if not it must be the wrong password.
+
+            conn.Close();
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
